2a8d601d33bf5b6aabacba56d753a64df2c566cc
marcin-listwan <marcin735@gmail.com>
2021-02-16 16:40:54 +0000

    non-responsive-version

---
+++ main.js
@@ -0,0 +1,29 @@
+//const blogPost = document.querySelector('.post-content--js');
+//console.log(blogPost);
+
+//blogPost.innerHTML = 'Dzisaj uczę się zmiany DOM-u za pomocą skryptu JS';
+
+
+/*function greetings(name, age){
+     console.log(`Witaj ${name}, wiem ze masz ${age} lat`);
+    return greetings;
+}
+*/
+
+/*function calculate(myNumber){
+    console.log(`dostalem numer ${myNumber}`);
+    return myNumber*3;
+
+}
+*/
+
+//main-section--js
+
+function changeP(querySelectorContent, newContent){
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = newContent;
+
+}
+
+changeP('.main-section--js', "Paragraf zostal zmieniony pomyslnie");
+
